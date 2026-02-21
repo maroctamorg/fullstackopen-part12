6c4e2d1cbaa9935bcd092c13baca0f46df8f5f5f
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2026-02-21 09:45:33 +0200

    exercise 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,15 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  req.todo.text = req.body.text
+  req.todo.done = req.body.done
+  await req.todo.save()
+  res.sendStatus(204);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
